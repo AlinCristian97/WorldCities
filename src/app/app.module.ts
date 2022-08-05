04c4c225ed869e16c30216d395bf1e2dc5f1ebe1
Alin Cristian <alin.cristian977@gmail.com>
2022-08-05 12:01:12 +0300

    tweaked proxy config and added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavMenuComponent
   ],
   imports: [
     BrowserModule, HttpClientModule
