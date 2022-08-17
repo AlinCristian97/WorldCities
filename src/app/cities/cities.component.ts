b51ff8833daa17acff17804f90f9d1d5daed3176
Alin Apostol <alin.cristian977@gmail.com>
2022-08-17 16:01:44 +0300

    updated cities component logic and display

--- src/app/cities/cities.component.ts
+++ src/app/cities/cities.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from './../../environments/environment';
+import { City } from './city';
 
 @Component({
   selector: 'app-cities',
@@ -6,10 +9,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cities.component.scss']
 })
 export class CitiesComponent implements OnInit {
+  public cities!: City[];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.http.get<City[]>(environment.baseUrl + 'api/Cities').subscribe(result => {
+      this.cities = result;
+    }, error => console.error(error));
   }
-
 }
